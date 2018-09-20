dc69732619bea2784645241e44975c6b5b7b009b
Saki-hanna <mme.eva.roux@gmail.com>
2018-09-20 18:31:52 +0200

    add test exception if not cat or no mouse

--- cat-and-mouse/src/main/java/catandmouse/services/MoveCatService.java
+++ cat-and-mouse/src/main/java/catandmouse/services/MoveCatService.java
@@ -8,7 +8,17 @@ public class MoveCatService {
      * @param position position du chat face à la souris
      * @return
      */
-    public String attack(String position){
+    public String attack(String position) throws Exception {
+        char cat = position.charAt(0);
+        if(cat != 'C'){
+            throw new Exception();
+        }
+
+        char mouse = position.charAt(position.length()-1);
+        if(mouse != 'm'){
+            throw new Exception();
+        }
+
         return "";
     }
 }
--- cat-and-mouse/src/test/java/catandmouse/services/CatAndMouseServiceTest.java
+++ cat-and-mouse/src/test/java/catandmouse/services/CatAndMouseServiceTest.java
@@ -2,44 +2,61 @@ package catandmouse.services;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CatAndMouseServiceTest {
     MoveCatService moveCatService = new MoveCatService();
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void noDotCaught() {
+    public void noDotCaught() throws Exception{
         String result = this.moveCatService.attack("Cm");
         assertEquals("Caught!", result);
     }
 
     @Test
-    public void OneDotCaught() {
+    public void OneDotCaught() throws Exception{
         String result = this.moveCatService.attack("C.m");
         assertEquals("Caught!", result);
     }
 
     @Test
-    public void TwoDotCaught() {
+    public void TwoDotCaught() throws Exception{
         String result = this.moveCatService.attack("C..m");
         assertEquals("Caught!", result);
     }
 
     @Test
-    public void threeDotCaught() {
+    public void threeDotCaught() throws Exception{
         String result = this.moveCatService.attack("C...m");
         assertEquals("Caught!", result);
     }
 
     @Test
-    public void fourDotEscaped() {
+    public void fourDotEscaped() throws Exception{
         String result = this.moveCatService.attack("C....m");
         assertEquals("Escaped!", result);
     }
 
-    @Test
-    public void thenDotEscaped() {
+    @Test()
+    public void thenDotEscaped() throws Exception{
         String result = this.moveCatService.attack("C..........m");
         assertEquals("Escaped!", result);
+    }
+
+    @Test
+    public void NoMouse() throws Exception{
+        thrown.expect(Exception.class);
+        this.moveCatService.attack("C..........");
+    }
+
+    @Test
+    public void NoCat() throws Exception{
+        thrown.expect(Exception.class);
+        this.moveCatService.attack("..........m");
     }
 }
